f4bb17fefa762172d455ceb6b8345bb08b92e362
Guilherme Ferreira <83267524+Guilhermeferreir@users.noreply.github.com>
2023-12-27 10:33:28 -0300

    autentication keycloak

---
+++ autentication_keycloak/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, g
+from flask_oidc import OpenIDConnect
+import json
+
+app = Flask(__name__)
+
+app.config.update(
+        SECRET_KEY='',
+        OIDC_CLIENT_SECRETS='keycloak.json',
+        OIDC_INTROSPECTION_AUTH_METHOD='client_secret_post',
+        OIDC_TOKEN_TYPE_HINT='access_token',
+        OIDC_SCOPES=['openid','email','profile'],
+        OIDC_OPENID_REALM='develop'
+    )
+
+oidc = OpenIDConnect(app)
+
+@app.route('/')
+def index():
+    if oidc.user_loggedin:
+        info = oidc.user_getinfo(["preferred_username", "email", "sub"])
+        return 'Welcome %s' % info.get("preferred_username")
+    else:
+        return '<h1>Not logged in</h1>'
+
+@app.route('/login')
+@oidc.require_login
+def login():
+    token = oidc.get_access_token()
+    info = oidc.user_getinfo(["preferred_username", "email", "sub"])
+    username = info.get("preferred_username")
+    return "Token: " + token + "<br/><br/>  Username: " + username
+
+@app.route('/logout')
+def logout():
+    oidc.logout()
+    return '<h2>Hi, you have been logged out! <a href="/">Return</a></h2>'
---
+++ autentication_keycloak/package_install.sh
@@ -0,0 +1,5 @@
+pip install Werkzeug==2.3.8
+pip install Flask==2.0.1
+pip install wheel==0.40.0
+pip install flask-oidc==1.4.0
+pip install itsdangerous==2.0.1
